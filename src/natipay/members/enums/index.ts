2193e25395392315b2a68313de3c26f12fe301a5
Mark Cecon <mark.cecon@gmail.com>
2025-03-05 19:33:41 -0300

    refactor: Rename Natipay Members enums for clarity

--- src/natipay/members/enums/index.ts
+++ src/natipay/members/enums/index.ts
@@ -1,2 +1,2 @@
-export { MemberRulesEnum } from './member-rules.enum';
-export { MemberTypeEnum } from './member-type.enum';
+export { NatipayMemberRulesEnum } from './member-rules.enum';
+export { NatipayMemberTypeEnum } from './member-type.enum';
