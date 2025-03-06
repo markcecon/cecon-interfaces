8b5034c7de6d7c4bc18579164a1c34f4255d1a3a
Mark Cecon <mark.cecon@gmail.com>
2025-03-06 13:57:32 -0300

    feat: Add new webhook types for Vision module in Chef Config

--- src/payio/chef-config/entities/chef-config-webhook.entity.ts
+++ src/payio/chef-config/entities/chef-config-webhook.entity.ts
@@ -11,7 +11,15 @@ export class PayioChefConfigWebhookEntity implements IPayioChefConfigWebhook {
   public response: string | null = null;
   public secret: string | null = null;
 
-  public type: 'PING' | 'GET_PRODUCT' | 'SEND_ORDER' | 'GET_ORDER' | 'CANCEL_ORDER' | 'GET_ORDER_STATUS' = 'SEND_ORDER';
+  public type:
+    | 'PING'
+    | 'GET_PRODUCT'
+    | 'SEND_ORDER'
+    | 'GET_ORDER'
+    | 'CANCEL_ORDER'
+    | 'GET_ORDER_STATUS'
+    | 'VISION_SEND_IMG'
+    | 'VISION_ASSIGN_POSITION' = 'SEND_ORDER';
   public url: string = '';
 
   // #endregion Properties (9)
--- src/payio/chef-config/interfaces/i-chef-config-webhook.ts
+++ src/payio/chef-config/interfaces/i-chef-config-webhook.ts
@@ -6,7 +6,15 @@ export interface IPayioChefConfigWebhook {
   authToken: string | null;
   secret: string | null;
   url: string;
-  type: 'PING' | 'GET_PRODUCT' | 'SEND_ORDER' | 'GET_ORDER' | 'CANCEL_ORDER' | 'GET_ORDER_STATUS' ;
+  type:
+    | 'PING'
+    | 'GET_PRODUCT'
+    | 'SEND_ORDER'
+    | 'GET_ORDER'
+    | 'CANCEL_ORDER'
+    | 'GET_ORDER_STATUS'
+    | 'VISION_SEND_IMG'
+    | 'VISION_ASSIGN_POSITION';
   response: string | null; // Resposta do webhook
   locked: boolean; // Trava caso não esteja respondendo
 
